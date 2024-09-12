f6e3fc8e0fccba8c60865f2bd2e47c7541b27cda
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-09-11 21:02:34 -0500

    rm snapshot folder and updated coverart test

--- src/__tests__/CoverArt.test.tsx
+++ src/__tests__/CoverArt.test.tsx
@@ -3,8 +3,8 @@ import { render } from '@testing-library/react';
 import CoverArt from '../components/CoverArt';
 
 describe('CoverArt Component', () => {
-    it('should match snapshot', () => {
-        const { asFragment } = render(<CoverArt />);
-        expect(asFragment()).toMatchSnapshot();
-    });
+  it('should match snapshot', () => {
+    const { asFragment } = render(<CoverArt />);
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
