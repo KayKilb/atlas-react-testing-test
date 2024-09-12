b72ca74965017cd511533f7463f89b7a4651a8ef
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-09-11 20:53:54 -0500

    file was in wrong file location

---
+++ src/__tests__/CoverArt.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import CoverArt from '../components/CoverArt';
+
+describe('CoverArt Component', () => {
+    it('should match snapshot', () => {
+        const { asFragment } = render(<CoverArt />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
